013bcbb50e1f21150657ff896bdc90d161f6257b
xxhls <heyq020814@qq.com>
2024-03-15 00:09:40 +0800

    :bug: remove process.cwd() to static

--- src/templates/index.ts
+++ src/templates/index.ts
@@ -1,4 +1,3 @@
-import fs from 'fs-extra';
 import path from 'path';
 import mit from './MIT';
 import apache2 from './Apache2';
@@ -7,12 +6,13 @@ import { error, info } from '../log';
 // 生成器类型
 type Generator = (name: string, email: string) => string;
 
+// 若要新增 License，1/3 需要在这里添加名称
 // 获取所有的 License 路径
-const licensesPath = fs.readdirSync(`${process.cwd()}/src/templates/`).filter(license => license !== 'index.ts');
+const licensesPath = ['Apache2.ts', 'MIT.ts'];
 // 获取所有的 License 名称
 const licenses = licensesPath.map(license => path.parse(license).name).map(license => license.toLowerCase());
 
-// 若要新增 License，1 需要在这里添加 License 枚举 & 对应的生成器
+// 若要新增 License，2/3 需要在这里添加 License 枚举 & 对应的生成器
 enum License {
   MIT,
   Apache2,
@@ -22,7 +22,7 @@ const licenseMap: { [key in License]: Generator } = {
   [License.Apache2]: apache2,
 };
 
-// 若要新增 License，2 需要在这里添加判断条件
+// 若要新增 License，3/3 需要在这里添加判断条件
 const getGenerator = (license: null | string) => {
   let generator: (typeof licenseMap)[License.MIT];
   if (!license) {
