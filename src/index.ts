889f350677c61d44c5f9002cf341bedf78b6778a
xxhls <heyq020814@qq.com>
2024-03-14 21:10:56 +0800

    set lint

--- src/index.ts
+++ src/index.ts
@@ -1,72 +1,47 @@
-import fs from "fs-extra";
-import getConfig from "./utils/getConfig";
-import { info, debug, warn, error } from "./log";
-import licenseMap from "./templates";
-import { License } from "./templates";
-import type { LicenseType } from "./templates";
-import { Command } from "commander";
+import fs from 'fs-extra';
+import getConfig from './utils/getConfig';
+import { info, debug, warn, error } from './log';
+import licenseMap, { License } from './templates';
+
+import { Command } from 'commander';
+import { name, version, description } from '../package.json';
 
 const main = async () => {
-    const program = new Command();
+  const program = new Command();
 
-    program
-        .name("get-license")
-        .description("Get License")
-        .version("0.1.7");
-    
-    program
-        .requiredOption("--license <license>", "Select License")
-        .parse(process.argv);
+  /**
+   * 设置命令行
+   *
+   * 1. 设置命令行名称
+   * 2. 设置命令行描述
+   * 3. 设置命令行版本
+   * 4. 设置命令行选项
+   * 5. 解析命令行参数
+   */
+  program
+    .name(name)
+    .description(description)
+    .version(version)
+    .requiredOption('--license <license>', '选择 License 类型, 可选值: mit')
+    .parse(process.argv);
 
-    const options = program.opts();
-    const { license } = options;
+  const options = program.opts();
+  const { license } = options;
 
-    if (!license) {
-        error("未选择 License 类型");
-        process.exit(1);
-    } else if (license === "mit" || license === "MIT"){
-        info("成功选择 MIT License");
-        const generator = licenseMap[License.MIT];
-        const config = await getConfig();
-        const licenseStr = generator(config.name, config.email);
-        const licensePath = `${process.cwd()}/LICENSE`;
-        fs.outputFileSync(licensePath, licenseStr);
-    } else {
-        error("未知的 License 类型");
-        process.exit(1);
-    }
-
-    // let generator: ((name: string, email: string) => string) | null = null;
-    // let license: LicenseType | null = null;
-    // const selectedLicense = (val: string) => {
-    //     if (val === "mit" || val === "MIT") {
-    //         info("成功选择 MIT License");
-    //         license = License.MIT;
-    //         generator = licenseMap[License.MIT];
-    //     } else {
-    //         error("未知的 License 类型");
-    //         process.exit(1);
-    //     }
-    // }
-
-    // const commander = new Command();
-    // commander
-    //     .option("--license <license>", "Select License", selectedLicense)
-    //     .parse(process.argv);
-    
-    // console.log("license", license);
-    // if (!license) {
-    //     error("未选择 License 类型");
-    //     process.exit(1);
-    // }
-    
-    // const config = await getConfig();
-
-    // if (!generator) { return 0;}
-    // const licenseStr = generator(config.name, config.email);
-
-    // const licensePath = `${process.cwd()}/LICENSE`;
-    // fs.outputFileSync(licensePath, licenseStr);
+  if (!license) {
+    error('未选择 License 类型');
+    process.exit(1);
+  } else if (license === 'mit' || license === 'MIT') {
+    info('成功选择 MIT License');
+    const generator = licenseMap[License.MIT];
+    const config = await getConfig();
+    const licenseStr = generator(config.name, config.email);
+    const licensePath = `${process.cwd()}/LICENSE`;
+    fs.outputFileSync(licensePath, licenseStr);
+  } else {
+    error('未知的 License 类型');
+    process.exit(1);
+  }
 };
 
 main();
